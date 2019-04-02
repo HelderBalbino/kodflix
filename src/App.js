299b125bdc0000779bd3559126a899b5939e024b
Helder Farias Balbino <HelderBalbino@Shivs-iPhone.localdomain>
2019-04-02 16:48:40 +0100

    Replace TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
-import lord from './images/lordoftherings.jpg';
 import './App.css';
+
+import lordOfTheRings from './images/Lordoftherings.jpg';
+import avengers from './images/avengers-infinity.jpg';
+import starwars from './images/Star Wars.jpg';
+import spiderMan from './images/spider-man.jpg';
+import batman from './images/Batman.jpg';
+import starTrek from './images/Star Trek.jpg';
+import limitless from './images/limitless.jpg';
+import godzilla from './images/godzilla.jpg';
+
+
 
 class App extends Component {
   render() {
     return (
+      
 <div className="App">
-      <img src={lord} alt="lord of the rings movie cover"/>
-      <br/> <br/>
 
-      <h1>Lord of the rings</h1>
+<div>
+      <img src={lordOfTheRings} alt='Lord of the rings'/>
+      <img src={avengers} alt='Avengers'/>
+      <img src={starwars} alt='Avengers'/>
+      <img src={spiderMan} alt='Spider man'/>
+      <img src={batman} alt='Batman'/>
+      <img src={starTrek} alt='Star Trek'/>
+      <img src={limitless} alt='limitless'/>
+      <img src={godzilla} alt='godzilla'/>
 
-    <div className="container">
-      <div className="movieName">moulin rouge</div>
-      <div className="movieName">avengers</div>
-      <div className="movieName">Star wars</div>
-      <div className="movieName">The lego movie</div>
+      
+
+      
+
+</div>
+
+
+
+    <div className='container'>
+      <div className='movieName'>The Lord of the Rings: The Fellowship of the Ring (2001)</div>
+      <div className='movieName'>Avengers: Infinity War (2018)</div>
+      <div className='movieName'>Star Wars: Episode IV - A New Hope (1977)</div>
     </div>
+
+    <div className='container'>
+      <div className='movieName'>Spider-man</div>
+      <div className='movieName'>Star Trek II: The Wrath of Khan (1982)</div>
+      <div className='movieName'>limitless</div>
+      <div className='movieName'>godzilla</div>
+    </div>
+
+     
       
 </div>
+
     );
   }
 }
