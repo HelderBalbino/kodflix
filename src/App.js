68e5dfc3ccf52dcb8ce2118959dd23e5ce03b79e
Helder Farias Balbino <HelderBalbino@helders-mbp.home>
2019-04-03 23:23:18 +0100

    Create a Gallery component to show the covers #9

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Movies from './Movies';
-import lordOfTheRings from './images/Lordoftherings.jpg';
-import avengers from './images/avengers-infinity.jpg';
-import starwars from './images/Star Wars.jpg';
-import spiderMan from './images/spider-man.jpg';
-import batman from './images/Batman.jpg';
-import starTrek from './images/Star Trek.jpg';
+import MoviesGallery from './MoviesGallery'
+
+
 
 
 
@@ -17,21 +13,7 @@ class App extends Component {
 <div className="App">
       <br/><br/><br/>
 
-  <div className='movieContainer'>
-
-        <Movies name='The Lord of the Rings: The Fellowship of the Ring (2001)' image={lordOfTheRings} />
-        <Movies name='Avengers: Infinity War (2018)' image={avengers} />
-        <Movies name='Star Wars: Episode IV - A New Hope (1977)' image={starwars} />
-
-  </div>
-
-  <div className='movieContainer'>
-
-        <Movies name='Spider-Man (2016)' image={spiderMan} />
-        <Movies name='Batman Begins (2005)' image={batman} />   
-        <Movies name='Star Trek II: The Wrath of Khan (1982)' image={starTrek} />
-      
-  </div>
+  <MoviesGallery />
 
     
 </div>
@@ -39,6 +21,11 @@ class App extends Component {
     );
   }
 }
+
+
+
+
+
 
 export default App;
 
---
+++ src/MoviesGallery.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+import Movies from './Movies';
+import lordOfTheRings from './images/Lordoftherings.jpg';
+import avengers from './images/avengers-infinity.jpg';
+import starwars from './images/Star Wars.jpg';
+import spiderMan from './images/spider-man.jpg';
+import batman from './images/Batman.jpg';
+import starTrek from './images/Star Trek.jpg';
+
+export default function MoviesGallery () {
+    return (  
+      <div>
+        <div className='movieContainer'>
+          <Movies name='The Lord of the Rings: The Fellowship of the Ring (2001)' image={lordOfTheRings} />
+          <Movies name='Avengers: Infinity War (2018)' image={avengers} />
+          <Movies name='Star Wars: Episode IV - A New Hope (1977)' image={starwars} />
+        </div>
+        < div className='movieContainer'>
+          <Movies name='Spider-Man (2016)' image={spiderMan} />
+          <Movies name='Batman Begins (2005)' image={batman} />   
+          <Movies name='Star Trek II: The Wrath of Khan (1982)' image={starTrek} />  
+        </div>
+    </div>
+  )
+}
