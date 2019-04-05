ee175eb8d81cf3b7504b3e96f754916ed70863cc
Helder Farias Balbino <HelderBalbino@Shivs-iPhone.localdomain>
2019-04-05 11:47:36 +0100

    Improve the gallery layout #11

--- src/MoviesGallery.js
+++ src/MoviesGallery.js
@@ -16,12 +16,11 @@ export default function MoviesGallery () {
           <Movies id='lordRings' name='The Lord of the Rings: The Fellowship of the Ring (2001)' image={lordOfTheRings} />
           <Movies id='avengers'  name='Avengers: Infinity War (2018)' image={avengers} />
           <Movies id='starWars'  name='Star Wars: Episode IV - A New Hope (1977)' image={starwars} />
-        </div>
-        < div className='movieContainer'>
           <Movies id='spiderman' name='Spider-Man (2016)' image={spiderMan} />
           <Movies id='batman'    name='Batman Begins (2005)' image={batman} />   
           <Movies id='starTrek'  name='Star Trek II: The Wrath of Khan (1982)' image={starTrek} />  
         </div>
+       
     </div>
   )
 }
